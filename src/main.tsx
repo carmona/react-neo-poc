84748a342d9bc79447f1b0fd62e78c6c25e0e971
Filipe Carmona <frcarmona@avaya.com>
2025-01-24 17:57:12 -0300

    fix(Contacts): fixed webcomponent exporting, added css files loader

--- src/main.tsx
+++ src/main.tsx
@@ -4,13 +4,21 @@ import { Provider } from 'react-redux';
 import { BrowserRouter } from 'react-router-dom';
 import App from './App.tsx';
 import { store } from './store/index.ts';
+import ContactsWidgetExport from './routes/Widgets/ContactsWidgetExport.tsx';
 
-ReactDOM.createRoot(document.getElementById('root')!).render(
-  <React.StrictMode>
-    <Provider store={store}>
-      <BrowserRouter>
-        <App />
-      </BrowserRouter>
-    </Provider>
-  </React.StrictMode>,
-);
+customElements.define('avaya-widget-mgm-contacts', ContactsWidgetExport);
+// eslint-disable-next-line no-console
+console.info('avaya-widget-mgm-contacts defined');
+
+const rootElement = document.getElementById('root');
+if (rootElement) {
+  ReactDOM.createRoot(document.getElementById('root')!).render(
+    <React.StrictMode>
+      <Provider store={store}>
+        <BrowserRouter>
+          <App />
+        </BrowserRouter>
+      </Provider>
+    </React.StrictMode>,
+  );
+}
--- src/routes/Widgets/ContactsWidgetExport.tsx
+++ src/routes/Widgets/ContactsWidgetExport.tsx
@@ -1,14 +1,27 @@
-import { createElement } from 'react';
-import { render, unmountComponentAtNode } from 'react-dom';
+import React from 'react';
+import { createRoot } from 'react-dom/client';
 import ContactsWidget from './ContactsWidget.tsx';
 
-class ContactsWidgetExport extends HTMLElement {
+export default class ContactsWidgetExport extends HTMLElement {
   connectedCallback() {
-    render(createElement(ContactsWidget), this);
+    const mountPoint = document.createElement('div');
+    const shadow = this.attachShadow({ mode: 'open' });
+    const style = document.createElement('style');
+    // TODO: change this URL to be dynamic
+    style.textContent = `
+      @import url("http://127.0.0.1:8080/mgm-contacts.css");
+      @import url("http://127.0.0.1:8080/mgm-contacts2.css");
+    `;
+    shadow.appendChild(style);
+    shadow.appendChild(mountPoint);
+
+    const root = createRoot(mountPoint);
+    root.render(<ContactsWidget />);
   }
 
   disconnectedCallback() {
-    unmountComponentAtNode(this);
+    const mountPoint = this.shadowRoot?.querySelector('div');
+    const root = createRoot(mountPoint as Element);
+    root.unmount();
   }
 }
-customElements.define('avaya-widget-mgm-contacts', ContactsWidgetExport);
