142ee24ffd8fc74194114d0db3b25f667d78858d
Filipe Carmona <frcarmona@avaya.com>
2025-01-28 13:31:15 -0300

    feat(Contacts): added webcomponent parameter support, updated json

--- src/routes/Widgets/ContactsWidgetExport.tsx
+++ src/routes/Widgets/ContactsWidgetExport.tsx
@@ -3,21 +3,27 @@ import { createRoot } from 'react-dom/client';
 import ContactsWidget from './ContactsWidget.tsx';
 
 export default class ContactsWidgetExport extends HTMLElement {
+  static get observedAttributes() {
+    return ['base-url'];
+  }
+
   connectedCallback() {
     const mountPoint = document.createElement('div');
     mountPoint.classList.add('mgm-contacts-widget-container');
     const shadow = this.attachShadow({ mode: 'open' });
     const style = document.createElement('style');
+    // 127 is the local dev server from ./dist directory
+    const baseUrl = this.getAttribute('base-url') || 'http://127.0.0.1:8080';
     // TODO: change this URL to be dynamic
     style.textContent = `
-      @import url("http://127.0.0.1:8080/mgm-contacts.css");
-      @import url("http://127.0.0.1:8080/mgm-contacts2.css");
+      @import url("${baseUrl}/mgm-contacts.css");
+      @import url("${baseUrl}/mgm-contacts2.css");
     `;
     shadow.appendChild(style);
     shadow.appendChild(mountPoint);
 
     const root = createRoot(mountPoint);
-    root.render(<ContactsWidget />);
+    root.render(<ContactsWidget baseUrl={baseUrl} />);
   }
 
   disconnectedCallback() {
